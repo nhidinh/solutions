af50310add058cccc4a11d7563bd00d7c722ba1d
thevency <46436668+thevency@users.noreply.github.com>
2019-01-28 11:14:53 +0700

    Update SearchTests.java

--- regression-tests/src/test/java/com/hansencx/portal/tests/SearchTests.java
+++ regression-tests/src/test/java/com/hansencx/portal/tests/SearchTests.java
@@ -5,8 +5,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import com.hansencx.solutions.logger.Log;
+import utilities.configuration.InitialData;
 import utilities.helper.ExcelHelper;
 import utilities.helper.FailureHandling;
+import utilities.helper.StringEncrypt;
 
 /**
  * @param
@@ -17,6 +19,8 @@ import utilities.helper.FailureHandling;
 
 
 public class SearchTests extends PortalBaseTest {
+    ExcelHelper excelHelper = new ExcelHelper();
+
     @Test(description = "Search by Enrollment Number With Filter 'in list' ")
     public void searchByEnrollmentNumberInList(){
         Page.TopNavigation().clickSearchButton();
@@ -25,48 +29,51 @@ public class SearchTests extends PortalBaseTest {
         Assert.assertEquals(Page.SearchResult().getNumberOfResult(),2 );
     }
 
-   // @BeforeTest
-//    public void setUpTestData(){
-//        String DataDirectory = "D:\\Users\\dinhn\\IdeaProjects\\solutions\\solutions\\regression-tests\\src\\test\\java\\com\\hansencx\\portal\\datatest\\";
-//        String DataFileName = "PortalDataTest.xlsx";
-//        String SheetName = "EnrollmentNumber";
-//        ExcelHelper.setupExcelTestData(DataDirectory, DataFileName, SheetName);
-//    }
+    @BeforeTest
+    public void setUpTestData(){
+        String DataDirectory = InitialData.PARENT_DIR +"\\regression-tests\\src\\test\\java\\com\\hansencx\\portal\\datatest\\";
+        String DataFileName = "PortalDataTest.xlsx";
+        String SheetName = "EnrollmentNumber";
 
-  //  @Test(description = "Search by Enrollment Number with Data File")
-//    public void searchByEnrollmentNumberWithDataFile(){
-//        int countRow = ExcelHelper.getNumberOfRow();
-//        String testcaseName;
-//        String filterOption;
-//        String enrollmentNumberValue;
-//        String result;
-//        int filterOptionCell = ExcelHelper.getCellIndexByText("Filter");
-//        int EnrollmentNumberValueCell = ExcelHelper.getCellIndexByText("Value");
-//        int tcNameCell = ExcelHelper.getCellIndexByText("TestCaseName");
-//        int resultCell = ExcelHelper.getCellIndexByText("Result");
-//
-//        for(int i = 1; i<countRow; i++){
-//            filterOption = ExcelHelper.getCellData(i, filterOptionCell);
-//            enrollmentNumberValue = ExcelHelper.getCellData(i, EnrollmentNumberValueCell);
-//            testcaseName = ExcelHelper.getCellData(i, tcNameCell);
-//            result = ExcelHelper.getCellData(i, resultCell);
-//            int resultValue = Integer.parseInt(result);
-//
-//            Page.TopNavigation().clickSearchButton();
-//            if(filterOption.equals("contains")){
-//                Page.Search().selectSupplierByName("Talen Energy Electric");
-//            }
-//            Page.Search().searchByEnrollmentNumberWithFilter(filterOption, enrollmentNumberValue);
-//            Page.Search().clickSearchButton();
-//            int numberOfResult = Page.SearchResult().getNumberOfResult();
-//            try {
-//                Assert.assertEquals(numberOfResult, resultValue);
-//            }catch (AssertionError e){
-//                FailureHandling.continueAtFailedTestCase(e, testcaseName);
-//            }
-//            Log.info("Complete Test case: "+ testcaseName);
-//            System.out.println("Compete Test case: " + testcaseName);
-//
-//        }
-//    }
+        excelHelper.setupExcelTestData(DataDirectory, DataFileName, SheetName);
+    }
+
+    @Test(description = "Search by Enrollment Number with Data File")
+    public void searchByEnrollmentNumberWithDataFile(){
+        int countRow = excelHelper.getNumberOfRow();
+        String testcaseName;
+        String filterOption;
+        String enrollmentNumberValue;
+        String result;
+        int filterOptionCell = excelHelper.getCellIndexByText("Filter");
+        int EnrollmentNumberValueCell = excelHelper.getCellIndexByText("Value");
+        int tcNameCell = excelHelper.getCellIndexByText("TestCaseName");
+        int resultCell = excelHelper.getCellIndexByText("Result");
+
+        for(int i = 1; i<countRow; i++){
+            filterOption = excelHelper.getCellData(i, filterOptionCell);
+            enrollmentNumberValue = excelHelper.getCellData(i, EnrollmentNumberValueCell);
+            testcaseName = excelHelper.getCellData(i, tcNameCell);
+            result = excelHelper.getCellData(i, resultCell);
+            int resultValue = Integer.parseInt(result);
+
+            Page.TopNavigation().clickSearchButton();
+            if(filterOption.equals("contains")){
+                Page.Search().selectSupplierByName("Talen Energy Electric");
+            }
+            Page.Search().searchByEnrollmentNumberWithFilter(filterOption, enrollmentNumberValue);
+            Page.Search().clickSearchButton();
+            int numberOfResult = Page.SearchResult().getNumberOfResult();
+            try {
+                Assert.assertEquals(numberOfResult, resultValue);
+            }catch (AssertionError e){
+                FailureHandling.continueAtFailedTestCase(e, testcaseName);
+            }
+            Log.info("Complete Test case: "+ testcaseName);
+            System.out.println("Compete Test case: " + testcaseName);
+
+
+        }
+    }
+
 }
