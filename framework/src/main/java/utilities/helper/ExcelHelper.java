00b4952c014f07979e6ca84d747cc5a8803e0092
nguyenv <vinguyen264@gmail.com>
2019-01-30 10:00:51 +0700

    new kw and excel modify

--- framework/src/main/java/utilities/helper/ExcelHelper.java
+++ framework/src/main/java/utilities/helper/ExcelHelper.java
@@ -1,9 +1,7 @@
 package utilities.helper;
 
 import com.hansencx.solutions.logger.Log;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -12,101 +10,236 @@ import org.openqa.selenium.Platform;
 import utilities.configuration.InitialData;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.apache.commons.io.FilenameUtils.separatorsToSystem;
+import static org.apache.poi.ss.usermodel.CellType.*;
 
 /**
  * @param
  * @author Nhi Dinh
  * @return
- * @since 1/15/2019
  * @update Jan 23, 2019
+ * @since 1/15/2019
  */
 public class ExcelHelper {
 
     private Platform platform = InitialData.PLATFORM;
-    private String testDataExcelPath;
+    private String filePath;
+    private String sheetName;
     private XSSFWorkbook excelWorkBook;
     private XSSFSheet excelSheet;
-    private XSSFCell cell;
-    private XSSFRow row;
-    private int rowNumber;
-    private int columnNumber;
+    private FileInputStream excelFile;
 
-    public int getRowNumber() {
-        return rowNumber;
-    }
-    public void setRowNumber(int rowNumber) {
-        this.rowNumber = rowNumber;
+    private DataFormatter formatter = new DataFormatter();
+
+    public ExcelHelper() {
     }
 
-    public int getColumnNumber() {
-        return columnNumber;
-    }
-    public void setColumnNumber(int columnNumber) {
-        this.columnNumber = columnNumber;
-    }
+    public ExcelHelper(String filePath, String sheetName) throws FileNotFoundException {
 
-    DataFormatter formatter = new DataFormatter();
-
-    public void setDataFileLocation(String dataDirectory, String testDataExcelFileName){
-        String dataFile = dataDirectory + testDataExcelFileName;
-        setTestDataExcelPath(separatorsToSystem(dataFile));
-        Log.info("Data File Location: " + testDataExcelPath + "\n");
+        this.filePath = filePath;
+        this.sheetName = sheetName;
+        openFile();
     }
 
-    public void setExcelFileSheet(String sheetName){
-        try{
-            //Open The Excel File:
+    /**
+     * open the excel ffile
+     *
+     * @param
+     * @return String value of the cell
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-29
+     */
+    public void openFile() {
+        try {
             Log.info("Opening the data file");
-            FileInputStream ExcelFile = new FileInputStream(testDataExcelPath);
+            excelFile = new FileInputStream(filePath);
             Log.info("Getting Excel File");
-            excelWorkBook = new XSSFWorkbook(ExcelFile);
+            excelWorkBook = new XSSFWorkbook(excelFile);
             Log.info("Setting Excel File Sheet");
-            excelSheet = excelWorkBook.getSheet(sheetName);
+            excelSheet = getExcelWorkBook().getSheet(sheetName);
+
+            FormulaEvaluator evaluator = getExcelWorkBook().getCreationHelper().createFormulaEvaluator();
+            CellValue cellValue = evaluator.evaluate(getExcelSheet().getRow(2).getCell(3));
+
             Log.info("Complete setting Excel File Sheet");
-        }catch (Exception e){
+
+        } catch (Exception e) {
             Log.error("FAILED to set Excel File Sheet");
             Log.error(e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public int getNumberOfRow(){
-        return excelSheet.getPhysicalNumberOfRows();
+    public void forceFormulaRecalculation() {
+        this.getExcelWorkBook().setForceFormulaRecalculation(true);
     }
 
-    public int getLastRowNum(){
-        return excelSheet.getLastRowNum();
+    /**
+     * close the excel ffile
+     *
+     * @param
+     * @return String value of the cell
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-29
+     */
+    public void closeFile() {
+        try {
+            excelFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getLastRowNum() {
+        return getExcelSheet().getLastRowNum();
+    }
+
+    public int getNumberOfRow() {
+        return getExcelSheet().getPhysicalNumberOfRows();
+    }
+
+    /**
+     * Return String of cell
+     *
+     * @param rowIndex, colIndex
+     * @return String value of the cell
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-30
+     */
+    public String getCellValue(int rowIndex, int colIndex) {
+        XSSFCell cell = getExcelSheet().getRow(rowIndex).getCell(colIndex);
+        if (null != cell) {
+            if (cell.getCellType() == FORMULA)
+                return cell.getRawValue();
+            else
+                return formatter.formatCellValue(cell);
+        }
+        return "";
+    }
+
+    public String getCellData(int rowNumber, int colNumber) {
+        Log.info("Getting Cell Data...");
+        XSSFCell cell = getExcelSheet().getRow(rowNumber).getCell(colNumber);
+        //DataFormatter formatter = new DataFormatter();
+        Log.info("Return Cell Data Value: " + cell);
+        return formatter.formatCellValue(cell);
+    }
+
+    /**
+     * Return String of cell
+     *
+     * @param xssfCelll
+     * @return String value of the cell
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-30
+     */
+    public String getCellValue(XSSFCell xssfCelll) {
+
+        String cellValue = "";
+        if (null != xssfCelll) {
+            switch (xssfCelll.getCellType()) {
+                case BOOLEAN:
+                    cellValue = xssfCelll.getBooleanCellValue() + "";
+                    break;
+                case NUMERIC:
+                    cellValue = xssfCelll.getRawValue() + "";
+                    break;
+                case STRING:
+                    cellValue = xssfCelll.getStringCellValue() + "";
+                    break;
+                case BLANK:
+                    cellValue = "";
+                    break;
+                case ERROR:
+                    cellValue = xssfCelll.getErrorCellValue() + "";
+                    break;
+
+                // CELL_TYPE_FORMULA will never occur
+                case FORMULA:
+                    break;
+            }
+
+        }
+        return cellValue;
+
+    }
+
+    /**
+     * Return list of cell values in a row
+     *
+     * @param rowIndex
+     * @return a String list of parameters of the specified row(step) of the test script (Excel)
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-23
+     */
+    public List<String> getRowValue(int rowIndex) {
+        List<String> listParameters = new ArrayList<>();
+
+        //argument cell is from cell 3 -> 7 of each row
+        for (int i = 3; i < 8; i++)
+            listParameters.add(getCellValue(rowIndex, i));
+        return listParameters;
+    }
+
+    /**
+     * Return list of cell values in a row
+     *
+     * @param rowIndex
+     * @return a XSSFCell list of parameters of the specified row(step) of the test script (Excel)
+     * @author Vi Nguyen
+     * @see
+     * @since 2019-01-30
+     */
+    public List<XSSFCell> getRow(int rowIndex) {
+        List<XSSFCell> listParameters = new ArrayList<>();
+        //argument cell is from cell 3 -> 7 of each row
+        for (int i = 3; i < 8; i++)
+            listParameters.add(getExcelSheet().getRow(rowIndex).getCell(i));
+        return listParameters;
     }
 
     public XSSFRow getRowData(int rowNumber) {
         Log.info("Getting Row Data...");
-        row = excelSheet.getRow(rowNumber);
+        XSSFRow row = getExcelSheet().getRow(rowNumber);
         Log.info("Return Row data value");
         return row;
     }
 
-    public void setCellData(String value, int rowNumber, int colNumber){
-        try{
-            row = excelSheet.getRow(rowNumber);
-            cell = row.getCell(colNumber);
+    public void setCellValue(int rowIndex, int colIndex, String value) {
+        XSSFRow row = getExcelSheet().getRow(rowIndex);
+        XSSFCell cell = row.getCell(colIndex);
+        if (null != cell)
+            cell.setCellValue(value);
+        else
+            row.createCell(colIndex).setCellValue(value);
+    }
+
+    public void setCellData(String value, int rowNumber, int colNumber) {
+        try {
+            XSSFRow row = getExcelSheet().getRow(rowNumber);
+            XSSFCell cell = row.getCell(colNumber);
 
             Log.info("Setting Cell Data...");
-            if(cell == null){
+            if (cell == null) {
                 cell = row.createCell(colNumber);
                 cell.setCellValue(value);
-            }else {
+            } else {
                 cell.setCellValue(value);
             }
-            FileOutputStream outputFile = new FileOutputStream(testDataExcelPath);
+            FileOutputStream outputFile = new FileOutputStream(filePath);
 
-            Log.info("Writing Data to file: "+outputFile);
-            excelWorkBook.write(outputFile);
+            Log.info("Writing Data to file: " + outputFile);
+            getExcelWorkBook().write(outputFile);
             outputFile.flush();
             outputFile.close();
 
@@ -118,10 +251,18 @@ public class ExcelHelper {
         }
     }
 
+    public XSSFWorkbook getExcelWorkBook() {
+        return excelWorkBook;
+    }
+
+    public XSSFSheet getExcelSheet() {
+        return excelSheet;
+    }
+
     public int getCellIndexByText(String text){
         Log.info("Getting cell index by text: " + text);
         DataFormatter formatter = new DataFormatter();
-        row = excelSheet.getRow(0);
+        XSSFRow row = excelSheet.getRow(0);
         int cellIndex = -1;
         if(row == null){
             Log.error("Header row is empty");
@@ -140,62 +281,5 @@ public class ExcelHelper {
             System.out.println("No cell is found in header");
         }
         return cellIndex;
-    }
-
-    public void setupExcelTestData(String DataDirectory,String testDataExcelName, String ExcelSheetName){
-        Log.info("Setting up Test Data");
-//        InitialData.getProjectDirectory();
-        setDataFileLocation(DataDirectory, testDataExcelName);
-        setExcelFileSheet(ExcelSheetName);
-    }
-
-    public void setTestDataExcelPath(String testDataExcelPath) {
-        this.testDataExcelPath = testDataExcelPath;
-    }
-
-    // Author: Vi
-    public String getCellValue(int rowIndex,  int colIndex){
-        cell = excelSheet.getRow(rowIndex).getCell(colIndex);
-        if(null != cell){
-            if (cell.getCellType() == CellType.FORMULA)
-                return cell.getRawValue();
-            else
-                return formatter.formatCellValue(cell);
-        }
-        return  "";
-    }
-    // Author: Nhi
-    public String getCellData(int rowNumber, int colNumber){
-        Log.info("Getting Cell Data...");
-        cell = excelSheet.getRow(rowNumber).getCell(colNumber);
-        //DataFormatter formatter = new DataFormatter();
-        Log.info("Return Cell Data Value: " + cell);
-        return formatter.formatCellValue(cell);
-    }
-
-    /**
-     * quit driver after running a test suite
-     * @author Vi Nguyen
-     * @param
-     * @return list of parameters of the specified row(step) of the test script (Excel)
-     * @since 2019-01-23
-     * @see
-     */
-    public List<String> getRowValue(int rowIndex){
-        List<String>  listParameters = new ArrayList<>();
-
-        //argument cell is from cell 3 -> 7 of each row
-        for(int i=3; i<8; i++)
-            listParameters.add(getCellValue(rowIndex, i));
-        return listParameters;
-    }
-
-    public void setCellValue(int rowIndex, int colIndex, String value){
-        row = excelSheet.getRow(rowIndex);
-        cell = row.getCell(colIndex);
-        if(null != cell)
-            cell.setCellValue(value);
-        else
-            row.createCell(colIndex).setCellValue(value);
     }
 }
