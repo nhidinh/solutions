b6b197dde4398c64ddb54cfbee00a06d98f27b2d
nguyenv <vinguyen264@gmail.com>
2019-02-01 16:14:52 +0700

    addded compareXML

--- regression-tests/src/test/java/com/hansencx/portal/tests/CompareXML.java
+++ regression-tests/src/test/java/com/hansencx/portal/tests/CompareXML.java
@@ -1,5 +1,6 @@
 package com.hansencx.portal.tests;
 
+import com.hansencx.portal.common.DataFilePathHandler;
 import com.hansencx.solutions.core.BaseTest;
 import org.testng.annotations.Test;
 import utilities.configuration.InitialData;
@@ -11,14 +12,14 @@ public class CompareXML extends BaseTest {
 
     @Test(description = "Compare xml")
     public void compareXml() throws Exception {
-        String originPath = InitialData.PARENT_DIR + "\\regression-tests\\src\\test\\java\\com\\hansencx\\portal\\datatest\\";
-        String diffFilePath = InitialData.DIFFERENCE_DIR_PATH + InitialData.DIFF_FILE_NAME;
+        String originPath = DataFilePathHandler.DATA_DIRECTORY_PATH;
+        String diffFilePath = InitialData.XML_DIFFERENCE_DIR_PATH + InitialData.XML_DIFF_FILE_NAME;
 
         XmlHelper xmlHelper = new XmlHelper();
         xmlHelper.createTempAndSplitAndDiffFolder();
 
-        xmlHelper.splitFile(originPath + "BillPrint_QA.xml", InitialData.SPLITTED_DIR_PATH, "Accounts", "Account", "AccountNumber");
-        xmlHelper.compare2XMLFiles(InitialData.SPLITTED_DIR_PATH + "5483077000.xml", originPath + "5483077000_Prod.xml", InitialData.DIFFERENCE_DIR_PATH + InitialData.DIFF_FILE_NAME);
+        xmlHelper.splitFile(originPath + "BillPrint_QA.xml", InitialData.XML_SPLITTED_DIR_PATH, "Accounts", "Account", "AccountNumber");
+        xmlHelper.compare2XMLFiles(InitialData.XML_SPLITTED_DIR_PATH + "5483077000.xml", originPath + "5483077000_Prod.xml", InitialData.XML_DIFFERENCE_DIR_PATH + InitialData.XML_DIFF_FILE_NAME);
 
         File file = new File(diffFilePath);
         if (file.exists())
